770d818196023ddadca31f003cfc3dd7842237ab
Gerardo Vargas <evc414@gmail.com>
2020-06-26 15:36:34 -0500

    FileUploader added

--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -3,11 +3,14 @@ import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { FirebaseContext } from '../../firebase'
 import { useNavigate } from 'react-router-dom'
+import FileUploader from 'react-firebase-file-uploader'
 
 const NuevoPlatillo = () => {
 
     //context con las operaciones de firebase
     const { firebase } = useContext(FirebaseContext)
+
+    console.log(firebase)
     
     //Hook para redireccionar
     const navigate = useNavigate()
@@ -126,12 +129,12 @@ const NuevoPlatillo = () => {
                         }
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen">Imagen</label>
-                            <input 
-                                className="shadow appearance-none border rounded w-full py-2 px-. text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            <FileUploader 
+                                accept='image/*'
                                 id="imagen"
-                                type="file"
-                                value={formik.values.imagen}
-                                onChange={formik.handleChange}
+                                name="imagen"
+                                randomizeFilename
+                                storageRef={firebase.storage.ref("productos")}
                             />
                         </div>
                         <div className="mb-4">
