2bf2a1a8e1f7577581c00c6c7a72812c92622fad
Gerardo Vargas <evc414@gmail.com>
2020-06-26 13:05:50 -0500

    erros message added

--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -52,8 +52,16 @@ const NuevoPlatillo = () => {
                                 placeholder="Nombre platillo"
                                 value={formik.values.nombre}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
                         </div>
+                        { formik.touched.nombre && formik.errors.nombre ?
+                            (<div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                                <p className="font-bold">Hubo un error</p>
+                                <p>{formik.errors.nombre}</p>
+                            </div>)
+                            : null
+                        }
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="precio">Precio</label>
                             <input 
@@ -64,8 +72,17 @@ const NuevoPlatillo = () => {
                                 min="0"
                                 value={formik.values.precio}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+
                             />
                         </div>
+                        { formik.touched.precio && formik.errors.precio ?
+                            (<div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                                <p className="font-bold">Hubo un error</p>
+                                <p>{formik.errors.precio}</p>
+                            </div>)
+                            : null
+                        }
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="categoria">Categoria</label>
                             <select
@@ -74,6 +91,8 @@ const NuevoPlatillo = () => {
                                 name="categoria"
                                 value={formik.values.categoria}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+
                             >
                                 <option value="">-- Seleccione --</option>
                                 <option value="desayuno">Desayuno</option>
@@ -82,10 +101,15 @@ const NuevoPlatillo = () => {
                                 <option value="bebida">Bebidas</option>
                                 <option value="postres">Postres</option>
                                 <option value="ensaladas">Ensaladas</option>
-
-
                             </select>
                         </div>
+                        { formik.touched.categoria && formik.errors.categoria ?
+                            (<div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                                <p className="font-bold">Hubo un error</p>
+                                <p>{formik.errors.categoria}</p>
+                            </div>)
+                            : null
+                        }
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen">Imagen</label>
                             <input 
@@ -104,9 +128,16 @@ const NuevoPlatillo = () => {
                                 placeholder="Descripcion del platillo"
                                 value={formik.values.descripcion}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             ></textarea>
                         </div>
-
+                        { formik.touched.descripcion && formik.errors.descripcion ?
+                            (<div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                                <p className="font-bold">Hubo un error</p>
+                                <p>{formik.errors.descripcion}</p>
+                            </div>)
+                            : null
+                        }
                         <input 
                             type="submit"
                             className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white font-bold"
