996cd6a9453fd94dcfbf57d15df82fecd7a89771
Gerardo Vargas <evc414@gmail.com>
2020-06-26 14:15:59 -0500

    firebase app added

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuWa_kxJg_EC4VEMv4wu9rGnub-QYT1YE",
+    authDomain: "restaurant-9c16a.firebaseapp.com",
+    databaseURL: "https://restaurant-9c16a.firebaseio.com",
+    projectId: "restaurant-9c16a",
+    storageBucket: "restaurant-9c16a.appspot.com",
+    messagingSenderId: "370634593807",
+    appId: "1:370634593807:web:38289762379163bf96757a",
+    measurementId: "G-522L6KS1ET"
+  };
+
+
+  export default firebaseConfig
