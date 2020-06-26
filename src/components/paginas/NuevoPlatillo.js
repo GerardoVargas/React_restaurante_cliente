fc01b8c86a46bfaef628703f75d99be7e8398eba
Gerardo Vargas <evc414@gmail.com>
2020-06-26 14:42:54 -0500

    insert data to db

--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -2,12 +2,15 @@ import React, {useContext} from 'react';
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { FirebaseContext } from '../../firebase'
+import { useNavigate } from 'react-router-dom'
 
 const NuevoPlatillo = () => {
 
     //context con las operaciones de firebase
     const { firebase } = useContext(FirebaseContext)
-    console.log(firebase)
+    
+    //Hook para redireccionar
+    const navigate = useNavigate()
 
     //validacion y leer los datos del formulario
     const formik = useFormik({
@@ -32,8 +35,15 @@ const NuevoPlatillo = () => {
                         .required('La descripcion es obligatoria'),
 
         }),
-        onSubmit: datos => {
-            console.log(datos)
+        onSubmit: platillo => {
+            try{
+                platillo.existencia = true
+                firebase.db.collection('productos').add(platillo)
+                 //redireccionar
+                 navigate("/menu")
+            }catch(error){
+                console.log(error)
+            }
         }
     })
 
